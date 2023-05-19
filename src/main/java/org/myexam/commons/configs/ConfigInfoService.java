01be2cf1399e9cf927c3f8894f3ba736aed3108f
MYL <124130225+githubMYL@users.noreply.github.com>
2023-05-19 13:58:36 +0900

    Update ConfigInfoService.java

--- src/main/java/org/myexam/commons/configs/ConfigInfoService.java
+++ src/main/java/org/myexam/commons/configs/ConfigInfoService.java
@@ -1,16 +1,11 @@
 package org.myexam.commons.configs;
 
-<<<<<<< HEAD
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-=======
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
->>>>>>> adminpage
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.myexam.entities.Configs;
-import org.myexam.repositories.ConfigsRepository;
+import org.koreait.entities.Configs;
+import org.koreait.repositories.ConfigsRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,10 +15,6 @@ public class ConfigInfoService {
     private final ConfigsRepository repository;
 
     public <T> T get(String code, Class<T> clazz) {
-<<<<<<< HEAD
-        Configs configs = repository.findById(code).orElse(null);
-
-=======
         return get(code, clazz, null);
     }
 
@@ -33,7 +24,6 @@ public class ConfigInfoService {
 
     public <T> T get(String code, Class<T> clazz, TypeReference<T> typeReference) {
         Configs configs = repository.findById(code).orElse(null);
->>>>>>> adminpage
         if (configs == null || configs.getValue() == null || configs.getValue().isBlank()) {
             return null;
         }
@@ -43,14 +33,10 @@ public class ConfigInfoService {
         ObjectMapper om = new ObjectMapper();
         T data = null;
         try {
-<<<<<<< HEAD
-            data = om.readValue(value, clazz);
-=======
 
             if (clazz == null) data = om.readValue(value, typeReference);
             else data = om.readValue(value, clazz);
 
->>>>>>> adminpage
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
