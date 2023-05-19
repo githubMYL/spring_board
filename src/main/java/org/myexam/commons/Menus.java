6104f46f6e884ff5b2bbefa8140a9fde73a126d4
githubMYL <feverdy86@gmail.com>
2023-05-19 11:55:34 +0900

    게시판 설정 작업 중

--- src/main/java/org/myexam/commons/Menus.java
+++ src/main/java/org/myexam/commons/Menus.java
@@ -1,5 +1,8 @@
 package org.myexam.commons;
 
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -15,12 +18,17 @@ public class Menus {
 
         // 게시판 하위 메뉴
         if (code.equals("board")) {
-            menus.add(new MenuDetail("게시판 목록", "/admin/board"));
-            menus.add(new MenuDetail("게시판 등록/수정", "/admin/board/register"));
-            menus.add(new MenuDetail("게시글 관리", "/admin/board/posts"));
+            menus.add(new MenuDetail("board","게시판 목록", "/admin/board"));
+            menus.add(new MenuDetail("register", "게시판 등록/수정", "/admin/board/register"));
+            menus.add(new MenuDetail("posts", "게시글 관리", "/admin/board/posts"));
         }
 
         return menus;
+    }
 
+    public static String getSubMenuCode(HttpServletRequest request) {
+        String URI = request.getRequestURI();
+//        System.out.println("pathInfo::::::::::::" + URI);
+        return URI.substring(URI.lastIndexOf('/') + 1);
     }
 }
--- src/main/java/org/myexam/controllers/admins/BoardController.java
+++ src/main/java/org/myexam/controllers/admins/BoardController.java
@@ -1,17 +1,24 @@
 package org.myexam.controllers.admins;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.myexam.commons.MenuDetail;
 import org.myexam.commons.Menus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller("AdminBoardController")
 @RequestMapping("/admin/board")
+@RequiredArgsConstructor
 public class BoardController {
+
+    private final HttpServletRequest request;
+
     /**
      * 게시판 목록
      *
@@ -19,14 +26,44 @@ public class BoardController {
      */
     @GetMapping
     public String index(Model model) {
-        commonProcess(model);
+
+        commonProcess(model, "게시판 목록");
 
         return "admin/board/index";
     }
 
-    private void commonProcess(Model model) {
+    /**
+     * 게시판 등록
+     * @return
+     */
+    @GetMapping("/register")
+    public String register(Model model) {
+
+        commonProcess(model, "게시판 등록");
+
+        return "admin/board/config";
+    }
+
+    @GetMapping("/{bid}/update")
+    public String update(@PathVariable String bid, Model model) {
+
+        commonProcess(model, "게시판 수정");
+
+        return "admin/board/config";
+    }
+
+    private void commonProcess(Model model, String title) {
+        String URI = request.getRequestURI();
+//        System.out.println("URI:::::::::::" + URI);
+
         // 서브 메뉴 처리
+        String submenuCode = Menus.getSubMenuCode(request);
+
+        model.addAttribute("subMenuCode",submenuCode);
         List<MenuDetail> submenus = Menus.gets("board");
         model.addAttribute("submenus", submenus);
+
+        model.addAttribute("pageTitle", title);
+        model.addAttribute("title", title);
     }
 }
